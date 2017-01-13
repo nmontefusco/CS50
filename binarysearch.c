cf979ea6cafed0799274418216076fcf4bfa8c4b
Nicholas Montefusco <nmontefusco@yahoo.com>
2017-01-13 07:54:39 -0500

    Create binarysearch.c

---
+++ binarysearch.c
@@ -0,0 +1,58 @@
+/** binary search algorithim, that operates on a sorted array of numbers.
+* efficient over linear search
+*/
+
+
+#include<cs50.h>
+#include<stdio.h>
+
+#define SIZE 8
+
+
+
+bool search(int needle, int haystack[],int size)
+{
+
+int min = 0;
+int max = size-1;
+
+while(min<=max)
+    {
+        int midpoint= (min+max)/2;
+     
+        if(haystack[midpoint]==needle)
+        {
+          return true;
+        }
+        else if(needle<haystack[midpoint])
+          {
+            max= midpoint - 1;
+          }
+        else if(needle>haystack[midpoint])
+        {
+            min = midpoint + 1;
+        }
+        
+        
+    }
+       
+           
+        
+        
+        return false;
+}
+
+int main(void)
+{
+    int numbers[SIZE] = {4,8,15,16,23,42,50,108};
+    printf("> ");
+    int n= get_int();
+    if(search(n,numbers,SIZE))
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+}
